0045ade792809d0bff44f0828198ba7036b1c995
Ryan Hayes <ryanhayes923@gmail.com>
2020-11-12 21:26:54 -0500

    User Input Improvements
    - Allows any number of keywords rather than just 10
    - The response() method now doesn't recursively call the startSearch()
    method, preventing an unintentional recursive loop

--- DealSeeker/src/Main.java
+++ DealSeeker/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Main implements Runnable {
 	
-	private static int timeframe, currIndex;
+	private static int timeframe;
 	private static String[] keywords;
 	private static Scanner scanner;
+	private static Thread thread;
 
 	public static void main(String[] args) {
 		startSearch();
@@ -16,24 +17,9 @@ public class Main implements Runnable {
 	}
 	
 	public static void startSearch() {
-		keywords = new String[10];
-		currIndex = 0;
 		scanner = new Scanner(System.in);
-		System.out.println("Enter keyword(s) to search for, separated by a comma and a space (maximum of 10 items)");
-		String[] items = scanner.nextLine().split(", ");
-		for (int i = 0; i < items.length; i++) {
-			if (currIndex < keywords.length) {
-				if (keywords[currIndex] == null) {
-					keywords[currIndex] = items[i];
-					currIndex++;		
-				} else {
-					currIndex++;
-				}
-			} else {
-				System.out.println("Too many items!");
-				return;
-			}			
-		}
+		System.out.println("Enter keyword(s) to search for, separated by a comma and a space");
+		keywords = scanner.nextLine().split(", ");			
 		System.out.println("Enter the amount of time in minutes that you want to wait in between searches");
 		try {
 			timeframe = convertToMilli(scanner.nextInt());
@@ -41,21 +27,34 @@ public class Main implements Runnable {
 			System.out.println("Invalid Time Input");
 			return;
 		}
-		Thread thread = new Thread(new Main());
+		thread = new Thread(new Main());
 		scanner.nextLine();
 		thread.start();	
 	}
 	
-	public static String response() {
-		System.out.println("Restart Search? Y/N");
-		String result = scanner.nextLine();
-		return result;
+	public static void response() {
+		boolean validResponse = false;
+		while (!validResponse) {
+			System.out.println("Restart Search? Y/N");
+			String result = scanner.nextLine();
+			if (result.startsWith("Y")) {
+				validResponse = true;
+			} else if (result.startsWith("N")) {
+				System.out.println("Closing Program...");
+				scanner.close();
+				System.exit(0);
+			} else {
+				System.out.println("Invalid Response");
+				response();
+			}
+		}
+		startSearch();
 	}
 	
 	@Override
 	public void run() {
 		try {
-			System.out.println("Waiting for " + timeframe/60000 + " minute(s)");
+			System.out.println("Waiting for " + timeframe/60000 + " minute(s)...");
 			Thread.sleep(timeframe);
 			System.out.println("Time is up, finding deals for: ");
 			for (int i = 0; i < keywords.length; i++) {
@@ -63,23 +62,12 @@ public class Main implements Runnable {
 					System.out.println(keywords[i] + " ");
 				}
 			}
-	
 			//Print results in JFrame
 			System.out.println();
 			System.out.println("DISPLAY SEARCH RESULTS HERE");
 			System.out.println();
 			
-			String response = response();
-
-			if (response.startsWith("Y")) {
-				startSearch();
-			} else if (response.startsWith("N")) {		
-				scanner.close();
-				System.exit(0);
-			} else {
-				System.out.println("Invalid Response");
-				response();
-			}
+			response();
 
 		} catch (InterruptedException e) {
 			e.getStackTrace();
