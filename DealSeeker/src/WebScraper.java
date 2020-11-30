19690ffb53cf0de87801a1bec25d8114f20c4e15
AraerTalsen <43824879+AraerTalsen@users.noreply.github.com>
2020-11-30 17:28:24 -0500

    Built Web Scraper

---
+++ DealSeeker/src/WebScraper.java
@@ -0,0 +1,72 @@
+package DealSeeker.src;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class WebScraper 
+{
+
+	//Make a static call to this method
+	public static String[] findThisDeal(String s)
+	{
+		String urlPrefix = "https://dealsea.com/search?q=";
+		String urlSufix = "&search_mode=Deals";
+		String mid = "";
+		
+		//Remove all problem characters/ replace spaces with +
+		for(int i = 0; i < s.length(); i++)
+		{
+			if(Character.isWhitespace(s.charAt(i)))
+				mid += "+";
+			else if(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i)))
+				mid += s.charAt(i);
+		}
+		
+		return scrapeWeb(urlPrefix + s + urlSufix);
+		
+	}
+	
+	//Pull deals from webpage
+	private static String[] scrapeWeb(String url)
+	{
+		String[] deals = new String[3];
+		
+		try
+		{
+			final Document doc = Jsoup.connect(url).get();//Pull html from page
+			
+			int i = 0;
+			
+			//Find all deals
+			for(Element e : doc.select(".dealcontent"))
+			{
+				if(i >= 3) break;//Return maximum of three deals
+				
+				Element span = null;
+				
+				if(e.select("span") != null)//If span element exits (this is where DealSea always lists if the deal is expired)
+					span = e.selectFirst("span");
+				
+				if(span == null || !span.hasClass("colr_red"))//If not expired, add to list
+				{
+					Element strong = e.selectFirst("strong");
+					deals[i] = strong.getElementsByTag("a").text();
+					System.out.println(deals[i]);
+					i++;
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return deals;
+	}
+
+}
+
+//select()
+//getElementsByTag()
